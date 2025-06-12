9289cb52d620cb097c7fd4fc54bd391befda1f20
hnghuy <hnghuy824@gmail.com>
2025-06-12 23:58:29 +0700

    Update Normalization32Bit.v

--- rtl/Normalization32Bit.v
+++ rtl/Normalization32Bit.v
@@ -1,5 +1,3 @@
-`timescale 1ns / 1ps
-
 module Normalization32Bit (
     input [24:0] res,
     input [7:0] exp_base,
@@ -9,36 +7,32 @@ module Normalization32Bit (
 );
     reg [24:0] normalized_result;
     integer i;
-    reg continue_shift;
 
     always @(*) begin
         normalized_result = res;
         exp_res = exp_base;
         overflow = 0;
+        man_res = 0;
 
-        if (normalized_result == 0) begin //explicitly handle 0 case for lower runtime
+        if (normalized_result == 0) begin // explicit zero
             man_res = 0;
             exp_res = 0;
             overflow = 0;
-        end 
-        else begin
-            if (normalized_result[24]) begin // If MSB is 1, shift right once and increment exponent
-                normalized_result = normalized_result >> 1;
-                exp_res = exp_base + 1;
-            end 
-            else begin // Shift left until bit 23 is 1 or exponent reaches 0, max 25 times
-                repeat (24) begin                
-                    if(normalized_result[23] == 0) begin
-                        normalized_result = normalized_result << 1;
-                        exp_res = exp_res - 1;
-                    end
-		        end		    
+        end
+        else if (normalized_result[24]) begin // MSB là 1, shift phải 1 lần, tăng exponent
+            normalized_result = normalized_result >> 1;
+            exp_res = exp_base + 1;
             man_res = normalized_result[23:0];
-                if(exp_res==8'b11111111) begin
-                     overflow=1;
-                end
+            if (exp_res == 8'b11111111) overflow = 1;
+        end
+        else begin
+            // Shift trái đến khi bit 23 là 1 (chuẩn hóa), giảm exponent
+            for (i = 0; i < 24 && normalized_result[23] == 0 && exp_res > 0; i = i + 1) begin
+                normalized_result = normalized_result << 1;
+                exp_res = exp_res - 1;
             end
+            man_res = normalized_result[23:0];
+            if (exp_res == 8'b11111111) overflow = 1;
         end
     end
 endmodule
-
